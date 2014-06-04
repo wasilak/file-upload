f22a55aa9b89b1c6a3b3ff24c90f7cf4cb0c2e34
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2014-06-04 12:48:08 +0200

    some more fixes...

--- src/Wasilak/FileUpload/Uploader.php
+++ src/Wasilak/FileUpload/Uploader.php
@@ -4,8 +4,10 @@ namespace Wasilak\FileUpload;
 
 class Uploader {
 	private $_endpoint = false;
+
 	public function __construct(\Wasilak\FileUpload\Endpoint $endpoint)
 	{
+		$this->_endpoint = $endpoint;
 	}
 
 	public function upload(array $params)
